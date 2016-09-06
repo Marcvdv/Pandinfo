bfddda2e56fc3012112db9629acf6772616447f9
FaerieRose <rosalynn5772-git@yahoo.com>
2016-09-06 19:09:03 +0200

    Changed package to nl.pandinfo

---
+++ src/main/java/nl/pandinfo/PandInfoApplication.java
@@ -0,0 +1,12 @@
+package nl.pandinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandInfoApplication.class, args);
+	}
+}
