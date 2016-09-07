01ba456151772e30bb627fce6eb68ec2ac6f6b2d
marcvdv <marccvanderven@gmail.com>
2016-09-07 13:01:29 +0200

    Nu invoer in DB mogelijk

--- src/main/java/nl/pandinfo/controller/WebController.java
+++ src/main/java/nl/pandinfo/controller/WebController.java
@@ -1,20 +1,37 @@
 package nl.pandinfo.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import com.example.domain.Person;
-//import com.example.service.PersonServiceImpl;
+import nl.pandinfo.domain.Pand;
+import nl.pandinfo.service.PandServiceImpl;
 
 @Controller
 public class WebController {
+	@Autowired
+	PandServiceImpl pandServiceImpl;
 	
 	@RequestMapping("/")
 	public String start(Model model){
+		model.addAttribute("pand", new Pand());
 		return "index";
+	}
+	
+	@RequestMapping("pandSave")
+	public String savePand(@ModelAttribute("pand")Pand pand, Model model){
+		System.out.println(pand);
+		pandServiceImpl.savePand(pand);
+		model.addAttribute("panden",pandServiceImpl.findAllPanden());
+		return "pand";
+	}
+
+	@RequestMapping("pand")
+	public String showPanden(Model  model){
+		model.addAttribute("panden",pandServiceImpl.findAllPanden());
+		return "pand";
 	}
 	
 	@RequestMapping("/rosalynn/**")
